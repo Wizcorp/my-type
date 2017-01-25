1bddd7a40eb580b66bd4742552fcaf95cdc633ec
Ron Korving <rkorving@wizcorp.jp>
2017-01-25 20:04:18 +0900

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,191 @@
+# My Type
+
+A minimalistic JavaScript type checker and object creation library.
+
+
+## Installation
+
+```sh
+npm install --save my-type
+```
+
+
+## This module is really my type
+
+There are many JS(ON) schema validators out there. I don't really like any of them (although there are so many, I've
+not possibly seen all of them, I must admit). This one is a bit different in a few key areas:
+
+- A modern API designed for ES6 users.
+- A tiny API that achieves the same as the crazy verbose ones.
+- Object creation based on the schema and its default values.
+- Very verbose exceptions (major time saver during development).
+- High performance assertions.
+
+
+## Example
+
+Let's start with a simple example to sell you the idea.
+
+```js
+const { object, array, string, number, int, bool, mixed, any } = require('my-type');
+
+const userIdType = string().regexp(/^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$/, 'UUID');
+
+const user = object({
+	id:            userIdType,
+	name:          string().length(3, 50),
+	age:           int().range(18, 150).optional(),
+	sex:           string().values(['M', 'F']),
+	isBanned:      bool().default(false),
+	score:         number().default(0).range(0, 100),
+	foobar:        mixed().types([number(), string()]),
+	lastSeenUsers: array(userIdType).length(0, 5)
+});
+
+user.create({ id: 'abc', name: 'Bob', sex: 'M', foobar: 3, lastBattles: [{ opponentId: 'def' }] });
+```
+
+
+## A modern API
+
+The API is exposed in a way that only really makes sense when you use destructuring. For each supported data type, there
+is exactly one function, which you can pull off the module.
+
+Example:
+
+```js
+const { string, number, int, bool, mixed, any, array, object } = require('my-type');
+```
+
+### Type creation
+
+```js
+string();       // create a string type
+number();       // create a number type
+int();          // create an integer type
+bool();         // create a boolean type
+mixed();        // create a type that is a combination of other types
+any();          // create a wildcard type
+array(type);    // create an array type (with children of a given type)
+object(props);  // create an object type (with properties of given types)
+```
+
+### Default values (all types)
+
+You can set up all types to have a default value
+
+```js
+string().default('Hello world');  // create a string type with a default value
+```
+
+### Set up a type to be optional (all types)
+
+```js
+string().optional();  // create a string type that does not have to be set (ie: undefined and null are allowed)
+```
+
+### Constraints
+
+```js
+string().length(min, max);       // create a string type with a min/max length
+string().regexp(/^[0-9a-f]+$/i); // create a string type with a regular expression constraint
+string().values(['yes', 'no']);  // create a string type with a limited set of possible values
+number().range(min, max);        // create a number type with a min/max value
+number().values(1, 1.5, 2);      // create a number type with a limited set of possible values
+int().range(min, max);           // create an integer type with a min/max value
+int().values(1, 2, 3);           // create an integer type with a limited set of possible values
+bool().values([true]);           // create a boolean type with a limited set of possible values
+array(type).length(min, max);    // create an array type with a min/max length
+```
+
+Default, Optional and Constraints can be chained in any order:
+
+```js
+string().default('Hello world').length(1, 50);
+number().range(0, 1).default(0);
+string().length(3, 10).optional();
+```
+
+### Arrays
+
+```js
+array(number());    // array of numbers
+```
+
+### Objects
+
+```js
+object({
+	id: int(),
+	name: string(),
+	foo: bool(),
+	bar: array(number())
+});
+```
+
+
+## Creating and updating objects
+
+Once you have set up a schema, you can easily use it as a template for new objects. Remember that as long as you use
+`my-type`, all properties will always be checked against the schema.
+
+Given a schema:
+
+```js
+const user = object({
+	id: int(),
+	name: string().default('Anonymous'),
+	isBanned: bool().default(false),
+	lastLogin: int().optional()
+});
+```
+
+You can create instances like this:
+
+```js
+const myDefaultUser = user.create({ id: 1 });
+
+// myDefaultUser: {
+//   id: 1,
+//   name: 'Anonymous',
+//   isBanned: false,
+//   lastLogin: undefined
+// }
+```
+
+And update them like this:
+
+```js
+user.update(myDefaultUser, { name: 'Bob' });
+
+// myDefaultUser: {
+//   id: 1,
+//   name: 'Bob',
+//   isBanned: false,
+//   lastLogin: undefined
+// }
+```
+
+
+## Error handling
+
+Whenever an error occurs, an exception will be thrown. The exception is an instance of `require('my-type').MyTypeError`.
+The error message already provides as much information as possible. For example, when a type didn't match, the expected
+type and the found type will both be shown. When length or range assertions failed, both the expectation and actual
+values will be shown.
+
+You can access the following properties on these error objects:
+
+As is usual on Error objects:
+
+- message: The message string (as is normal on Error objects)
+- stack: The stack trace
+
+And additionally:
+
+- value: The value that broke the assertion rules.
+
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,426 @@
+'use strict';
+
+const deepCopy = require('deep-copy');
+
+function getType(value) {
+	if (value === null) {
+		return 'null';
+	}
+
+	if (Array.isArray(value)) {
+		return 'array';
+	}
+
+	return typeof value;
+}
+
+
+class MyTypeError extends TypeError {
+	constructor(message, value) {
+		super(message);
+
+		this.name = 'MyTypeError';
+		this.value = value;
+
+		this._baseMessage = message;
+		this._valueName = '';
+
+		this.updateMessage();
+	}
+
+	addParentProperty(name) {
+		if (typeof name === 'number') {
+			// array index
+			this._valueName = `[${name}]${this._valueName}`;
+		} else {
+			// object property name
+			this._valueName = `.${name}${this._valueName}`;
+		}
+
+		this.updateMessage();
+	}
+
+	updateMessage() {
+		this.message = this._baseMessage.
+			replace('%type', getType(this.value)).
+			replace('%name', this._valueName || 'Value').
+			replace('%value', this.value).
+			replace('%length', this.value && this.value.hasOwnProperty('length') ? this.value.length : 'undefined');
+	}
+}
+
+
+class Test {
+	constructor(failure, message, userLabel) {
+		this.failure = failure;
+		this.message = message;
+		this.userLabel = userLabel;
+	}
+
+	toString() {
+		let message = this.message;
+
+		if (this.userLabel) {
+			message = `${message} (${this.userLabel})`;
+		}
+
+		return `  if (${this.failure}) { throw new MyTypeError(${JSON.stringify(message)}, value); }`;
+	}
+}
+
+
+class Type {
+	constructor() {
+		this.tests = [];
+		this.assertFn = undefined;
+		this.isOptional = false;
+		this.defaultValue = undefined;
+	}
+
+	optional() {
+		this.isOptional = true;
+		this.assertFn = undefined;
+		return this;
+	}
+
+	addTest(failure, message, userLabel) {
+		this.tests.push(new Test(failure, message, userLabel));
+		this.assertFn = undefined;
+	}
+
+	assert(value) {
+		if (!this.assertFn) {
+			const optionalTest = this.isOptional ?
+				'  if (value === undefined || value === null) { return; }\n' :
+				new Test('value === undefined || value === null', '%name is not optional');
+
+			const tests = [optionalTest].concat(this.tests);
+
+			this.assertFn = new Function('value', 'MyTypeError', tests.join('\n')); // eslint-disable-line no-new-func
+		}
+
+		this.assertFn(value, MyTypeError);
+	}
+}
+
+
+class ScalarType extends Type {
+	values(values, userLabel) {
+		values = JSON.stringify(values);
+		this.addTest(`!${values}.includes(value)`, `%name must be one of ${values}`, userLabel);
+		return this;
+	}
+
+	default(value) {
+		if (value !== null && typeof value === 'object') {
+			throw new MyTypeError('The default value is not a scalar type (found: %type)', value);
+		}
+
+		this.defaultValue = value;
+		return this;
+	}
+}
+
+
+class StringType extends ScalarType {
+	constructor() {
+		super();
+		this.addTest("typeof value !== 'string'", '%name is not a string (found: %type)');
+	}
+
+	length(min, max, userLabel) {
+		if (typeof min === 'number') {
+			this.addTest(`value.length < ${min}`, `%name string length must be >= ${min} (found: %length)`, userLabel);
+		}
+
+		if (typeof max === 'number') {
+			this.addTest(`value.length > ${max}`, `%name string length must be <= ${max} (found: %length)`, userLabel);
+		}
+
+		return this;
+	}
+
+	regexp(re, userLabel) {
+		re = re.toString();
+		this.addTest(`!${re}.test(value)`, `%name does not match regular expression: ${re}`, userLabel);
+		return this;
+	}
+}
+
+
+class NumberType extends ScalarType {
+	constructor() {
+		super();
+		this.addTest("typeof value !== 'number'", '%name is not a number (found: %type))');
+	}
+
+	range(min, max, userLabel) {
+		if (typeof min === 'number') {
+			this.addTest(`value < ${min}`, `%name must be >= ${min} (found: %value)`, userLabel);
+		}
+
+		if (typeof max === 'number') {
+			this.addTest(`value > ${max}`, `%name must be <= ${max} (found: %value)`, userLabel);
+		}
+
+		return this;
+	}
+}
+
+
+class IntType extends ScalarType {
+	constructor() {
+		super();
+		this.addTest('!Number.isInteger(value)', '%name is not an integer (found: %type)');
+	}
+
+	range(min, max, userLabel) {
+		if (typeof min === 'number') {
+			this.addTest(`value < ${min}`, `%name must be >= ${min} (found: %value)`, userLabel);
+		}
+
+		if (typeof max === 'number') {
+			this.addTest(`value > ${max}`, `%name must be <= ${max} (found: %value)`, userLabel);
+		}
+
+		return this;
+	}
+}
+
+
+class BooleanType extends ScalarType {
+	constructor() {
+		super();
+		this.addTest("typeof value !== 'boolean'", '%name is not a boolean (found: %type)');
+	}
+}
+
+
+class AnyType extends Type {}
+
+
+class MixedType extends Type {
+	constructor() {
+		super();
+		this.subTypes = [];
+	}
+
+	types(types) {
+		this.subTypes = types;
+		return this;
+	}
+
+	assert(value) {
+		super.assert(value);
+
+		if (value !== undefined) {
+			let lastError;
+
+			for (const type of this.subTypes) {
+				try {
+					type.assert(value);
+					return; // success!
+				} catch (error) {
+					lastError = error;
+				}
+			}
+
+			if (lastError) {
+				throw lastError;
+			}
+		}
+	}
+}
+
+
+class ArrayType extends Type {
+	constructor(elementType) {
+		super();
+		this.elementType = elementType;
+		this.addTest('!Array.isArray(value)', '%name is not an array (found: %type)');
+	}
+
+	default(value) {
+		if (!Array.isArray(value)) {
+			throw new MyTypeError('The default value is not an array (found: %type)', value);
+		}
+
+		this.defaultValue = deepCopy(value);
+		return this;
+	}
+
+	length(min, max, userLabel) {
+		if (typeof min === 'number') {
+			this.addTest(`value.length < ${min}`, `%name array length must be >= ${min} (found: %length)`, userLabel);
+		}
+
+		if (typeof max === 'number') {
+			this.addTest(`value.length > ${max}`, `%name array length must be <= ${max} (found: %length)`, userLabel);
+		}
+
+		return this;
+	}
+
+	assert(value) {
+		super.assert(value);
+
+		for (let i = 0; i < value.length; i += 1) {
+			try {
+				this.elementType.assert(value[i]);
+			} catch (error) {
+				error.addParentProperty(i);
+				throw error;
+			}
+		}
+	}
+}
+
+
+class ObjectType extends Type {
+	constructor(propTypes) {
+		super();
+		this.addTest("value === null || typeof value !== 'object'", '%name is not an object (found: %type)');
+		this.propTypes = propTypes;
+		this.propNames = Object.keys(propTypes);
+
+		this.createFn = undefined;
+	}
+
+	default(value) {
+		if (!value || typeof value !== 'object') {
+			throw new MyTypeError('The default value is not an object (found: %type)', value);
+		}
+
+		this.defaultValue = deepCopy(value);
+		return this;
+	}
+
+	assert(value) {
+		super.assert(value);
+
+		for (const prop of this.propNames) {
+			try {
+				this.propTypes[prop].assert(value[prop]);
+			} catch (error) {
+				error.addParentProperty(prop);
+				throw error;
+			}
+		}
+	}
+
+	create(data) {
+		if (!this.createFn) {
+			const body = [];
+
+			for (const prop of this.propNames) {
+				const value = this.propTypes[prop].defaultValue;
+
+				if (value === undefined) {
+					body.push(`  ${JSON.stringify(prop)}: undefined`);
+				} else {
+					body.push(`  ${JSON.stringify(prop)}: ${JSON.stringify(value)}`);
+				}
+			}
+
+			this.createFn = new Function(`return {\n${body.join(',\n  ')}\n};`); // eslint-disable-line no-new-func
+		}
+
+		const obj = this.createFn();
+		this.update(obj, data);
+		return obj;
+	}
+
+	update(obj, data) {
+		const unhandledProperties = {};
+		for (const prop of this.propNames) {
+			unhandledProperties[prop] = true;
+		}
+
+		if (data && obj) {
+			if (typeof obj !== 'object') {
+				throw new MyTypeError('The given object to update is not an object-type (found: %type)', obj);
+			}
+
+			if (typeof data !== 'object') {
+				throw new TypeError('The given update-data is not an object-type (found: %type)', data);
+			}
+
+			const propNames = Object.keys(data);
+
+			// first assert all properties are valid
+
+			for (const prop of propNames) {
+				const type = this.propTypes[prop];
+
+				if (!type) {
+					throw new MyTypeError(`Unknown property "${prop}"`, data[prop]);
+				}
+
+				unhandledProperties[prop] = false;
+
+				try {
+					type.assert(data[prop]);
+				} catch (error) {
+					error.addParentProperty(prop);
+					throw error;
+				}
+			}
+
+			// safely assign all properties
+
+			for (const prop of propNames) {
+				obj[prop] = data[prop];
+			}
+		}
+
+		// check all properties that were not affected by the data-object
+
+		for (const prop of this.propNames) {
+			if (unhandledProperties[prop]) {
+				const type = this.propTypes[prop];
+
+				try {
+					type.assert(obj[prop]);
+				} catch (error) {
+					error.addParentProperty(prop);
+					throw error;
+				}
+			}
+		}
+	}
+}
+
+
+exports.MyTypeError = MyTypeError;
+
+exports.string = function () {
+	return new StringType();
+};
+
+exports.number = function () {
+	return new NumberType();
+};
+
+exports.int = function () {
+	return new IntType();
+};
+
+exports.bool = function () {
+	return new BooleanType();
+};
+
+exports.any = function () {
+	return new AnyType();
+};
+
+exports.mixed = function () {
+	return new MixedType();
+};
+
+exports.object = function (propTypes) {
+	return new ObjectType(propTypes);
+};
+
+exports.array = function (elementType) {
+	return new ArrayType(elementType);
+};
